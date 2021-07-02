2ba7620987c8503b76635ec104a73f6f92b4c1c2
Micah Hausler <mhausler@amazon.com>
2021-07-02 16:45:11 -0400

    Added unifi and wireguard config

---
+++ unifi/on-boot.d/20-wireguard.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Copy to /mnt/data/on_boot.d/20-wireguard.sh
+
+CONTAINER=wireguard
+
+# Starts a wireguard container that is deleted after it is stopped.
+# All configs stored in /mnt/data/wireguard
+if podman container exists ${CONTAINER}; then
+  podman start ${CONTAINER}
+else
+  podman run -i -d --rm --net=host --name ${CONTAINER} --privileged \
+    -v /mnt/data/wireguard:/etc/wireguard \
+    -v /dev/net/tun:/dev/net/tun \
+    -e LOG_LEVEL=info -e WG_COLOR_MODE=always \
+    masipcat/wireguard-go:latest-arm64v8
+fi
+
